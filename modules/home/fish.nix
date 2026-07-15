f0fbdb9f6bd73b13d6fbf6b0640aea176518f4c4
riolu <bluechicken18846@gmail.com>
2026-07-14 21:59:39 -0400

    adjust paths from /etc to /home

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -7,8 +7,6 @@
     enable = true;
     functions.fish_greeting.body = "pokeget random --hide-name";
     shellAliases = {
-      "cfg" = "sudo nvim /etc/nixos/hosts/t14s/configuration.nix";
-      "hme" = "sudo nvim /etc/nixos/hosts/t14s/home.nix";
       "nrs" = "nh os switch";
       "nrb" = "nh os boot";
       "ngc" = "sudo nh clean all";
