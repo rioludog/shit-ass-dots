2ff13893b66f7518fee83c68793e31cc10e9fde0
riolu <bluechicken18846@gmail.com>
2026-07-16 14:22:56 -0400

    update nvim config

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -5,8 +5,42 @@
       plugins = {
           bufferline.enable = true;
 	        lualine.enable = true;
-        };
+	        mini-pairs.enable = true;
+	        mini-snippets.enable = true;
+	        mini-icons.enable = true;
+	        mini-basics = {
+	          enable = true;
+	          settings = {
+              move_with_alt = true;
+	          };
+	        };
+	        mini-cursorword.enable = true;
+	        mini-starter.enable = true;
+	        notify.enable = false;
+	        mini-notify.enable = true;
+	        hardtime.enable = true;
 
+	        treesitter = {
+	          enable = true;
+	          settings = {
+	          indent.enable = false;
+	          highlight.enable = true;
+	          };
+	        };
+
+	        blink-cmp = {
+	          enable = true;
+	          setupLspCapabilities = true;
+	          settings = {
+	            completion = {
+	              documentation.auto_show = true;
+	            };
+	          #  sources.default = [ "lsp" "path" "snippets" ];
+	          #  signature.enabled = true;
+	            keymap.preset = "super-tab";
+	          };
+	        };
+        };
       opts = {
       	number = true;
 	      relativenumber = true;
